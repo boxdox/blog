897e92c9630b9ce55b5c4d5182874e9df5292d32
boxdox <19517445+boxdox@users.noreply.github.com>
2021-05-02 23:03:48 +0530

    fixed: flexbox bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,7 +58,9 @@ if (backToTopButton && mainHeight / innerHeight > 3) {
 }
 
 document
-  .querySelectorAll('article > img, article > figure > picture > img, article > p > img, figure.post--cover img')
+  .querySelectorAll(
+    'article > img, article > figure > picture > img, article > p > img, figure.post--cover img'
+  )
   .forEach(image => {
     image.addEventListener('click', () => {
       showLightbox({ imgUrl: image.src, altText: image.alt })
